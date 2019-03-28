70ba632df1ad55a33360fbe36b4acbfd0c02941e
jpliu2 <jpliu2@iflytek.com>
2019-03-28 16:42:30 +0800

    H5修改滚动

--- UrlProjectDemo/js/index.js
+++ UrlProjectDemo/js/index.js
@@ -114,9 +114,8 @@ var app = new Vue({
             // $('body').css('overflow','hidden')
             var ua = navigator.userAgent.toLowerCase()
             if(ua.match(/MicroMessenger/i) == "micromessenger") {
-                document.body.addEventListener('touchmove', function (e) {
-                  e.preventDefault(); //阻止默认的处理方式(阻止下拉滑动的效果)
-                }, {passive: false});
+                $('html').css('overflow','hidden')
+                $('body').css('overflow','hidden')
                 $('#coverBackview').css('display','block')
             }
         }
