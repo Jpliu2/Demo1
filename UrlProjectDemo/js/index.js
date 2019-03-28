6e8860a48e4481a8b0eca308ab3549d0810c6d53
jpliu2 <jpliu2@iflytek.com>
2019-03-28 15:49:34 +0800

    H5修改滚动

--- UrlProjectDemo/js/index.js
+++ UrlProjectDemo/js/index.js
@@ -112,11 +112,14 @@ var app = new Vue({
         checkWechat: function () {
             var ua = navigator.userAgent.toLowerCase()
             if(ua.match(/MicroMessenger/i) == "micromessenger") {
+                alert('微信')
                 document.body.addEventListener('touchmove', function (e) {
                     e.preventDefault(); //阻止默认的处理方式(阻止下拉滑动的效果)
                 }, {passive: false});
                 $('#coverBackview').css('display','block')
-            } 
+            }  else {
+                alert('浏览器')
+            }
         }
     }
 })
