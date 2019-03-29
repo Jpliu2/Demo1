cc0c043b8e73641e3420ad86d8e1a3db5c54697e
jpliu2 <jpliu2@iflytek.com>
2019-03-29 11:11:55 +0800

    判断应用从什么机器中打开

--- sharehomeworkinfo/js/index.js
+++ sharehomeworkinfo/js/index.js
@@ -105,15 +105,6 @@ var app = new Vue({
             if (flag) {
                 this.isTestApi = true
             }
-        },
-
-        //打开应用
-        openApp: function () {
-            var u = navigator.userAgent, app = navigator.appVersion
-            var isAndroid = u.indexOf('Android') > -1 || u.indexOf('Linux') > -1 //android终端或者uc浏览器
-            var isiOS = !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/) //ios终端
-            alert('是否是Android：'+isAndroid)
-            alert('是否是iOS：'+isiOS)
         }
     }
 })
